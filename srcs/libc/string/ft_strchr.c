550051c33218c768294a1c026c2041b68807d071
thomas loyan <thomas.loyan@gmail.com>
2022-12-03 03:23:37 +0100

    refacto ft_strchr: compare iterator with string len are really bad on big string, prefer return the value as soon as it is found

--- srcs/libc/string/ft_strchr.c
+++ srcs/libc/string/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: thloyan <thloyan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 16:35:50 by thloyan           #+#    #+#             */
-/*   Updated: 2022/12/01 16:35:51 by thloyan          ###   ########.fr       */
+/*   Updated: 2022/12/03 03:16:03 by thloyan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ char	*ft_strchr(const char *s, int c)
 	unsigned long	i;
 
 	if (c == 0)
-		return ((char *)&s[ft_strlen(s)]);
+		return ((char *)(s + ft_strlen(s)));
 	i = 0;
-	while (s[i] != (unsigned char)c && s[i] != 0)
-		i++;
-	if (i == ft_strlen(s))
-		return ((void *)0);
-	return ((char *)&s[i]);
+	while (s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)(s + i));
+		i = i + 1;
+	}
+	return (NULL);
 }
